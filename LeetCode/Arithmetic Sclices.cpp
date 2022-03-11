eaeb6f2869215e0fed03da84ea1eeadeaca2493c
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-03-11 21:18:49 +0530

    leetcode

--- LeetCode/Arithmetic Sclices.cpp
+++ LeetCode/Arithmetic Sclices.cpp
@@ -1,17 +1,21 @@
-class Solution {
-public:
-    int numberOfArithmeticSlices(vector<int>& nums) {
-        if(nums.size()<3)return 0;
-        int c=0;
-        int n=nums.size();
-        for(int i=0;i<n-2;i++){
-            int d=nums[i+1]-nums[i];
-            for(int j=i+2;j<n;j++){
-                if(nums[j]-nums[j-1]==d)++c;
-                else break;
+#include<bits/stdc++.h>
+using namespace std;
+void isPalindrom(string s) {
+        string k="";
+        for(char i: s){
+            if((i>='a' and i<='z') or (i>='A' and i<='Z') ){
+                k+=tolower(i);
             }
         }
-        return c;
-        
+        cout<<k<<endl;
     }
-};
+
+int main(){
+    string s="0P";
+    
+    isPalindrome(s);
+    
+    
+    
+    return 0;
+}
--- LeetCode/Longest Palindrome.cpp
+++ LeetCode/Longest Palindrome.cpp
@@ -1,20 +1,46 @@
-class Solution {
-public:
-    int longestPalindrome(string s) {
-        int c=0, o=0;
-        map<char, int>m;
-        for(char i:s)m[i]++;
-        
-        for(auto i:m){
-            if(i.second>=2){
-                c+=(floor(i.second/2)*2);
-                if(i.second%2==1)o++;
-            }
-            else if(i.second>0)o++;
-        }
-        
-        if(o>0)return c+1;
-        else return c;
-        
-    }
-};
+// C++ program for implementation of Bubble sort
+#include <bits/stdc++.h>
+using namespace std;
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+// A function to implement bubble sort
+int bubbleSort(int arr[], int n)
+{
+    int i, j;
+    int c=0;
+    for (i = 0; i < n-1; i++)   
+    
+    // Last i elements are already in place
+    for (j = 0; j < n-i-1; j++)
+        if (arr[j] > arr[j+1])
+            swap(&arr[j], &arr[j+1]), c++;
+    return c;
+}
+
+/* Function to print an array */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+// Driver code
+int main()
+{
+    int arr[] = {4, 9, 8, 2, 1};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int r=bubbleSort(arr, n);
+    cout<<"Sorted array: \n";
+    cout<<r;
+    return 0;
+}
+
+// This code is contributed by rathbhupendra
