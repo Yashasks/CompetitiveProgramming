42003ae1456671cc41772743b7b448b91bda6054
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-10-25 20:03:51 +0530

    Time: 163 ms (11.01%), Space: 80.7 MB (12.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int f(string s1, string s2, int i, int j, vector<vector<int>> &dp){
-        if(i<0)return j+1;
         if(j<0)return i+1;
+        if(i<0)return j+1;
+        
         if(dp[i][j]!=-1)return dp[i][j];
         if(s1[i]==s2[j])return dp[i][j]=f(s1, s2, i-1, j-1, dp);
         return dp[i][j]= min(
