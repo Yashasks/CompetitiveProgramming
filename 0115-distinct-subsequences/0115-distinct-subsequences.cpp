4fce98bc3d22190a91eccdc62fa9f93dc9682c82
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-10-25 15:54:16 +0530

    Time: 44 ms (80.36%), Space: 6.4 MB (96.01%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,20 +3,23 @@ public:
     int numDistinct(string s, string t) {
         int n=s.size();
         int m=t.size();
-        vector<vector<double>> dp(n+1, vector<double>(m+1, -1));
+        // vector<vector<double>> dp(n+1, vector<double>(m+1, -1));
+        vector<double> prev(m+1, 0), cur(m+1, 0);
         
-        for(int i=0;i<=m;i++)dp[0][i]=0;
+//         for(int i=0;i<=m;i++)dp[0][i]=0;
 
-        for(int i=0;i<=n;i++)dp[i][0]=1;
+//         for(int i=0;i<=n;i++)dp[i][0]=1;
         
+        prev[0]=cur[0]=1;
         //recurrence
         for(int i=1;i<=n;i++){
             for(int j=1;j<=m;j++){
-                if(s[i-1]==t[j-1])dp[i][j]=dp[i-1][j]+dp[i-1][j-1];
-                else dp[i][j]=dp[i-1][j];
+                if(s[i-1]==t[j-1])cur[j]=prev[j]+prev[j-1];
+                else cur[j]=prev[j];
             }
+            prev=cur;
         }
-        return dp[n][m];
+        return prev[m];
 
     }
 };
