c192ca856274047dd39514d83a45844cd598174d
Yashas K S <60478602+Yashasks@users.noreply.github.com>
2022-10-26 19:35:07 +0530

    Time: 235 ms (49.96%), Space: 73.3 MB (9.80%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -3,7 +3,7 @@ public:
     int f(string &s1, string &s2, int i, int j, vector<vector<int>> &dp){
         if(i<0 or j<0)return 0;
         if(dp[i][j]!=-1)return dp[i][j];
-        if(s1[i]==s2[j])return 1+f(s1, s2, i-1, j-1, dp);
+        if(s1[i]==s2[j])return dp[i][j]=1+f(s1, s2, i-1, j-1, dp);
         
         return dp[i][j]=max(f(s1, s2, i-1, j, dp), f(s1, s2, i, j-1, dp));
     }
